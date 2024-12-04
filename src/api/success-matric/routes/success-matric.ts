e9611c2d25c3a1ed663a4405a660fb3f0172fb6c
vien.maithe <vien.maithe@vienmaithe.local>
2024-12-04 16:19:32 +0700

    feat: update

--- src/api/success-matric/routes/success-matric.ts
+++ src/api/success-matric/routes/success-matric.ts
@@ -4,4 +4,23 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::success-matric.success-matric');
+export default factories.createCoreRouter('api::success-matric.success-matric', {
+  prefix: '',
+  only: ['find', 'findOne'],
+  except: [],
+  config: {
+    find: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    create: {},
+    update: {},
+    delete: {},
+  },
+});
